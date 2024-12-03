2b4a6afb27ec25e924295878358642bce2b43b89
Diego Barrueta <diego.barrueta@gmail.com>
2024-12-03 13:08:33 -0300

    main

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,6 @@ import * as path from 'path';
 async function bootstrap() {
   let httpsOptions = null;
 
-  if (process.env.ENVIRONMENT === 'development') {
-    httpsOptions = {
-      key: fs.readFileSync(path.join(__dirname, '..', 'certs', 'server.key')),
-      cert: fs.readFileSync(path.join(__dirname, '..', 'certs', 'server.cert')),
-    };
-  }
-
   if (process.env.ENVIRONMENT === 'production') {
     httpsOptions = {
       key: fs.readFileSync(
@@ -24,6 +17,13 @@ async function bootstrap() {
       cert: fs.readFileSync(
         '/etc/letsencrypt/live/api.reports.dmit.ar/fullchain.pem',
       ),
+    };
+  }
+
+  if (process.env.ENVIRONMENT === 'development') {
+    httpsOptions = {
+      key: fs.readFileSync(path.join(__dirname, '..', 'certs', 'server.key')),
+      cert: fs.readFileSync(path.join(__dirname, '..', 'certs', 'server.cert')),
     };
   }
 
